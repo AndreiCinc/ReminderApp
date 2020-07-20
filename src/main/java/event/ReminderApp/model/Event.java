bba00281679a1013ca1e3f34787f7d7aa9ea2fbd
Andrei Cinc <andrei.cinc9@gmail.com>
2020-07-20 16:59:41 +0300

    added event test

--- src/main/java/event/ReminderApp/model/Event.java
+++ src/main/java/event/ReminderApp/model/Event.java
@@ -25,6 +25,7 @@ public class Event {
         this.details = details;
     }
 
+
     public UUID getId(){
         return id;
     }
--- src/test/java/event/ReminderApp/model/EventTest.java
+++ src/test/java/event/ReminderApp/model/EventTest.java
@@ -1,13 +1,31 @@
 package event.ReminderApp.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Date;
+import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 class EventTest {
 
-    @Test
-    void getId() {
+    private Event event;
+
+    @BeforeEach
+    void setEvent() {
+        event = new Event(
+                UUID.fromString("b17759ed-ca4d-412a-b575-5c39dafb5355"),
+                "robert",
+                new Date(2020-2-2),
+                new Date(2020-2-2),
+                "Bam, first test");
     }
 
     @Test
