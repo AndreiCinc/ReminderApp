c2d2170e0182f5dc1178aed0112755651b7fa090
Andrei Cinc <andrei.cinc9@gmail.com>
2020-07-22 10:55:29 +0300

    changed fe architecture

--- reminderapp-fe/src/App.js
+++ reminderapp-fe/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import logo from './template/logo.svg';
 import './App.css';
-import './component/button/createButton.style.css';
+import title from './component/input/calendar.component.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <button class="createButton">Button</button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {title()}
     </div>
   );
 }
---
+++ reminderapp-fe/src/component/input/title.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './title.style.css';
+
+
+export default function title() {
+
+	return(
+		<div class="md-form active-cyan active-cyan-2 mb-3">
+		  <input class="form-control" type="text" placeholder="Insert Title..." aria-label="Search"/>
+		</div>
+	);
+}
