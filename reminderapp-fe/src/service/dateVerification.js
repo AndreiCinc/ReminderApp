fe175c19cb986ed232b3ded2b9e9a8b8b51d20d1
Andrei Cinc <andrei.cinc9@gmail.com>
2020-08-10 19:02:55 +0300

    deleted some console logs

--- reminderapp-fe/src/service/dateVerification.js
+++ reminderapp-fe/src/service/dateVerification.js
@@ -3,8 +3,6 @@
  const DateVerification = (date) => {
 	let actualDate = new Date().toString();
 	let memoDate = new Date(date).toString();
-	console.log(actualDate);
-	console.log(memoDate);
 	if (memoDate === actualDate ) {
 		return true;
 	}
