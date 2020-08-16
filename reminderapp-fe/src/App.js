ba1e66f72cb3c9bd62a5456548c9a573dc7b49d2
Andrei Cinc <andrei.cinc9@gmail.com>
2020-08-16 13:17:23 +0300

    changed first page to register page

--- reminderapp-fe/src/App.js
+++ reminderapp-fe/src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 import List from './component/list/list.component.js';
 import CreateEvent from './component/createEvent/createEvent.component.js';
 import LogIn from './component/logIn/logIn.component.js';
-
+import Register from './component/register/register.component.js';
 
 function App() {
 
   return (
     <div className="App">
-      <div className="Title">
-        <h1>ReminderApp</h1>
-      </div>
       <div>
-       <LogIn />
-      </div>
-      <div>
+       <Register/>
       </div>
     </div>
   );
