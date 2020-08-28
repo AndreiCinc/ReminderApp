0dd1c0bacf439b3abb6376d0bb460db9b7cfb27d
Andrei Cinc <andrei.cinc9@gmail.com>
2020-08-28 20:43:44 +0300

    Create authentication service class

---
+++ reminderapp-be/src/main/java/event/ReminderApp/service/AuthenticationService.java
@@ -0,0 +1,70 @@
+package event.ReminderApp.service;
+
+import event.ReminderApp.dao.UserInterface;
+import event.ReminderApp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+
+import io.jsonwebtoken.*;
+
+import java.util.Date;
+
+@Service
+public class AuthenticationService {
+
+    public final UserInterface userInterface;
+
+    private static final String SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
+
+    @Autowired
+    public AuthenticationService(@Qualifier("PersonDatabase") UserInterface userInterface) {
+        this.userInterface = userInterface;
+    }
+
+    public String Authenticate(User user) throws Exception {
+
+        User dbUser =  userInterface.getUserByEmail(user.getUserEmail()).get();
+
+        if(user.getUserEmail().compareTo(dbUser.getUserEmail()) != 0 ||
+                user.getUserPassword().compareTo(dbUser.getUserPassword()) != 0){
+            throw new Exception("Failed to authenticate");
+        }
+        return createJWT(String.valueOf(dbUser.getUserId()), dbUser.getUserName(), dbUser.getUserEmail(),86400000);
+    }
+
+    public String createJWT(String id, String issuer, String subject, long ttlMillis) {
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+        if (ttlMillis > 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+        return builder.compact();
+    }
+
+    public Claims decodeJWT(String jwt) {
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(jwt).getBody();
+    }
+
+}
+
