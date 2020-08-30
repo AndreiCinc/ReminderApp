52af0270706047cd7524f8f74d2ee35c1edf17b1
Andrei Cinc <andrei.cinc9@gmail.com>
2020-08-30 14:21:29 +0300

    Check if the cookie exists to access main page

--- reminderapp-fe/src/MainPage/MainPage.js
+++ reminderapp-fe/src/MainPage/MainPage.js
@@ -2,16 +2,36 @@ import React from 'react';
 import List from '../List/List.js';
 import '../LogIn/LogIn.css';
 import CreateEvent from '../CreateEvent/CreateEvent.js';
+import Cookie from '../Service/CookiesService.js';
+import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
+
 
 export default function MainPage(props) {
-	
+
+	let check = Cookie.checkLogin();
+
 	return(
-		<div>
-			<div className="Title">
-    			<h2>ReminderApp</h2>
-    		</div>
-			<List props={props.id}/>
-			<CreateEvent />
-		</div>
+		check ? 
+			<div>
+				<div className="Title">
+	    			<h2>ReminderApp</h2>
+	    		</div>
+				<List props={props.id}/>
+				<CreateEvent />
+			</div>	: 
+			<div>
+				<div className="Title">
+					<h2>ReminderApp</h2>
+				</div>
+				<div className="insert">
+				<h2>Insert your credentials, please!</h2>
+				</div>
+				<div style={{"margin-left": '50%'}}>
+				<Link to="/logIn">
+					<h3>LogIn</h3>
+				</Link>
+				</div>
+			</div>
+		
 	)
 }
